2a8b342dba49993ce656129ffd54636a8058ae9f
myaccount4coursera <miguems+coursera@gmail.com>
2015-07-26 13:06:36 +0200

    Completed Programming assignment 2 for Coursera RProg

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit to caching 
+## the inverse of a matrix rather than compute it repeatedly.
+## The following two functions will allow us to cache the inverse of a matrix.
 
-## Write a short comment describing this function
 
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(val) inv <<- val
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
+
+
+## EXAMPLE
+# create matrix: normal and cached
+mdat <- matrix(c(1,5,3, 0,12,13, 3, 4, 5), nrow = 3, ncol = 3, byrow = TRUE)
+mcm <- makeCacheMatrix(mdat)
+
+#solve non-cached
+solve(mdat)
+#solve cached
+cacheSolve(mcm)
+
+
